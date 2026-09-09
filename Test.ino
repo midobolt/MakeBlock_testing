80b02583498726549b8ffbd90d7750fc4fd89ef9
Mido <34779315+midobolt@users.noreply.github.com>
2026-09-09 12:46:36 +0200

    Add line following and obstacle detection functions
    Implement line following and obstacle avoidance logic.

---
+++ Test.ino
@@ -0,0 +1,55 @@
+#include <kaulab.h>
+
+int lineread;
+void FollowLine(){
+  
+  while(true){
+  lineread = zRobotGetLineSensor();
+
+  if (lineread == 2){
+    zRobotSetMotorSpeed(1, 0 );
+    zRobotSetMotorSpeed(2, 150);
+  } else if(lineread == 1){
+    zRobotSetMotorSpeed(1, -150);
+    zRobotSetMotorSpeed(2, 0 );
+  } else if(lineread == 0){
+    zRobotSetMotorSpeed(1, -150);
+    zRobotSetMotorSpeed(2, 150);
+  } else if (lineread == 3){
+    zRobotSetMotorSpeed(1, -90);
+    zRobotSetMotorSpeed(2, 0);    
+  }
+  }
+} 
+void Obstacle(){
+  distance = zRobotGetUltraSensor();
+  if(distance > 0 && distance < 20){
+    zRobotSetMotorSpeed(1, );
+    zRobotSetMotorSpeed(2, )
+  } else {
+    FollowLine();
+  }
+}
+// 3 = no line
+// 0 = both on line
+// 2 = right on line
+// 1 = left on line
+
+// 1 = Right motor
+// 2 = left motor
+
+void setup() {
+  // put your setup code here, to run once:
+  zInitialize();
+  zRobotSetMotorSpeed(1, -70);
+  zRobotSetMotorSpeed(2, 0);
+
+  zScheduleTask(Obstacle(), 50, 1000);
+  zScheduleTask(FollowLine(), 70, 1000);
+  
+}
+
+
+
+void loop() {
+}
